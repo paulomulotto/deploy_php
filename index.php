f9f07a0ba697e8375f7637ce24c2f6efd57ff703
Paulo Mulotto <paulo.mulotto@gmail.com>
2021-02-02 15:05:45 -0300

    novo site bootstrap

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 fw-normal">Paulo Mulotto</h1>
-      <p class="lead fw-normal">Subtitulo do meu site</p>
+      <p class="lead fw-normal">Para entrar em contato <a href="mailto:paulo.mulotto@gmail.com">clique aqui</a></p>
       <a class="btn btn-outline-secondary" href="#">Coming soon</a>
     </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
